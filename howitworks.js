8f0c4ba0de906fbe1e39588bb0c22c7ce33ace62
hppod <hppodgornik@gmail.com>
2020-08-27 22:29:32 -0300

    event queue e priorização da execução das funções

--- howitworks.js
+++ howitworks.js
@@ -1,25 +1,13 @@
-function fn1() {
-    fn2()
-}
-function fn2() {
-    fn3()
-}
-function fn3() {
-    setTimeout(() => fn4(), 3000)
-    console.log('fn3')
+function esperarPor(tempo) {
+    const futuro = Date.now() + tempo
+    while (Date.now() < futuro) { }
 }
 
-function fn4() {
-    fn5()
-}
+setTimeout(() => console.log('Exec #01'), 400)
+setTimeout(() => {
+    esperarPor(3000)
+    console.log('Exec #02')
+}, 300)
 
-function fn5() {
-    console.log('timeout')
-}
-
-function main() {
-    fn1()
-}
-
-main()
-console.log('fim')
+esperarPor(5000)
+console.log('Fim')
