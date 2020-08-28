12e33cd285ff3a2709e1f4723300dcf4aba2337a
hppod <hppodgornik@gmail.com>
2020-08-27 22:24:30 -0300

    pilha de execução na stack js

---
+++ howitworks.js
@@ -0,0 +1,25 @@
+function fn1() {
+    fn2()
+}
+function fn2() {
+    fn3()
+}
+function fn3() {
+    setTimeout(() => fn4(), 3000)
+    console.log('fn3')
+}
+
+function fn4() {
+    fn5()
+}
+
+function fn5() {
+    console.log('timeout')
+}
+
+function main() {
+    fn1()
+}
+
+main()
+console.log('fim')
