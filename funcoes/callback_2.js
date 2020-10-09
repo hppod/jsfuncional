8dd5f686d45fafecba033f40bb0f359f96f4fe66
hppod <hppodgornik@gmail.com>
2020-10-09 20:37:32 -0300

    Entendendo sobre a execução das callbacks

--- funcoes/callback_2.js
+++ funcoes/callback_2.js
@@ -3,8 +3,16 @@ const path = require('path')
 
 const caminho = path.join(__dirname, 'dados.txt')
 
-function exibirConteudo(err, conteudo) {
+function exibirConteudo(_, conteudo) {
     console.log(conteudo.toString())
 }
 
-fs.readFile(caminho, {}, exibirConteudo)
+console.log('Inicio...')
+fs.readFile(caminho, exibirConteudo)
+fs.readFile(caminho, (_, conteudo) => console.log(conteudo.toString()))
+console.log('Fim...')
+
+console.log('Inicio Sync...')
+const conteudo = fs.readFileSync(caminho)
+console.log(conteudo.toString())
+console.log('Fim Sync...')
