afb5a87aef5cb39e7382973adc59046b24d261ab
hppod <hppodgornik@gmail.com>
2020-10-09 20:27:18 -0300

    Callbacks 1

--- funcoes/callback_1.js
+++ funcoes/callback_1.js
@@ -1,9 +1,25 @@
+// callback está associado ao evento de acontecer algo, e quando acontecer executar algo 
+
 function exec(fn, a, b) {
     return fn(a, b)
 }
 
 const somarNoTerminal = (x, y) => console.log(x + y)
 const subtrairNoTerminal = (w, z) => console.log(w - z)
+const subtrair = (w, z) => w - z
 
 exec(somarNoTerminal, 56, 38)
 exec(subtrairNoTerminal, 182, 27)
+
+const r = exec(subtrair, 50, 25)
+console.log(r)
+
+const cb = () => console.log('Exec 1...')
+
+setInterval(cb, 1000)
+
+setInterval(() => console.log('Exec 2...'), 2000)
+
+setInterval(function () {
+    console.log('Exec 3...')
+}, 3000)
